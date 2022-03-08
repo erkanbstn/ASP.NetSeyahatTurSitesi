c06f4b807d81fdcb9e983f213485f2316631bae4
Erkan Bostan <profesyonel59@gmail.com>
2022-03-08 05:11:27 +0300

    ASP.NetSeyahatKurumsalSite
    Bir Seyahat Acentesinin Kurumsal Ayağına Uygun ASP.Net Web Projesi

---
+++ TurSite/TurSite/Services.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TurSite.Entity;
+
+namespace TurSite
+{
+    public partial class Services : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Repeater1.DataSource = Baglanti.db.TServices.Take(6).ToList();
+            Repeater1.DataBind();
+
+        }
+    }
+}
